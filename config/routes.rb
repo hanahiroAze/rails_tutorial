1ec2c542a46cb035da337dee8346d088e939fcc3
hanahiroAze <hanai@hitomedia.jp>
2017-03-04 19:09:13 +0900

    3.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  resources :microposts
-  resources :users
-  root 'users#index'
+  get  'static_pages/about'
+  get  'static_pages/contact'
+  # resources :microposts
+  # resources :users
 end
